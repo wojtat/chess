1a8683f2544f2fdd2533b3f9304ece96557ffb3c
Tilhon, Vojtěch <tilhovoj@fel.cvut.cz>
2021-05-12 20:27:40 +0200

    Moved and renamed the resource folder. Made sure that maven build works correctly

--- chess/src/main/java/cz/cvut/fel/pjv/tilhovoj/chess/gui/GuiResourceManager.java
+++ chess/src/main/java/cz/cvut/fel/pjv/tilhovoj/chess/gui/GuiResourceManager.java
@@ -28,7 +28,7 @@ public class GuiResourceManager {
     private final void createPieceImages() {
     	pieceImages = new BufferedImage[2][ChessPieces.values().length];
         try {
-            BufferedImage bi = ImageIO.read(GuiResourceManager.class.getResource("/images/pieces.png"));
+            BufferedImage bi = ImageIO.read(GuiResourceManager.class.getResource("/pieces.png"));
             for (int color = 0; color < 2; ++color) {
                 for (int i = 0; i < ChessPieces.values().length; i++) {
                 	int w, h;
